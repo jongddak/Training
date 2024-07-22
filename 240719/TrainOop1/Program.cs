54b1e21a0381ecb35ee876c6e22c91875390ed45
jongddak <hfsmsj316@gmail.com>
2024-07-22 11:01:10 +0900

    test
    test

---
+++ 240719/TrainOop1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainOop1
+{
+    class Plyer { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
