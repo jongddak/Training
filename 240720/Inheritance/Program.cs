627cf9f170687e5b8f0b25c8dec914718088408a
jongddak <hfsmsj316@gmail.com>
2024-07-23 19:38:16 +0900

    총기 클래스 및 메인 구현

--- 240720/Inheritance/Program.cs
+++ 240720/Inheritance/Program.cs
@@ -9,34 +9,168 @@ namespace Inheritance
     internal class Program
     {
 
-        public class Gun {
+        public abstract class Gun {
 
             public string Name;
             public string Ammotype;
+            public int AmmoMax;
             public int Damage;
             public double Firerate;
-            bool AttachScope;
+            public bool AttachScope;
+            public bool AttachGrip;
+            public bool AttachSuprresor;
 
-            public virtual void shoot() 
-            {
-                Console.WriteLine("총알을 발사합니다");
+            public virtual void Shoot() {
+                Console.WriteLine($"{Ammotype}탄이 {Firerate}초에 한발씩 발사됨");
             }
-            public virtual void reload()
-            {
-                Console.WriteLine("총알을 재장전합니다");
+            public virtual void Reload() {
+                Console.WriteLine($"{Ammotype}탄이 {AmmoMax}발 장전됨");
             }
-            public virtual void adjustment()
+            public abstract void Attachment();
+        }
+
+        public class Aug : Gun {
+
+            public Aug(string name, string ammotype, int ammoMax, int damage, double firerate, bool attachScope, bool attachGrip, bool attachSuprresor)
             {
-                Console.WriteLine("조정간을 조정합니다");
+                Name = name;
+                Ammotype = ammotype;
+                AmmoMax = ammoMax;
+                Damage = damage;
+                Firerate = firerate;
+                AttachScope = attachScope;
+                AttachGrip = attachGrip;
+                AttachSuprresor = attachSuprresor;
+
+                Console.WriteLine($"{Ammotype}탄을 사용하는 {Name}입니다 ");
+            }
+
+
+            public override void Attachment()
+            {
+                Console.Write($"{Name}에 ");
+
+                if (AttachScope == true)
+                {
+                    Console.Write($"8배율스코프 장착 ");
+                }
+
+                if (AttachGrip == true)
+                {
+                    Console.Write($"수직손잡이 장착 ");
+                }
+                if (AttachSuprresor == true)
+                {
+                    Console.Write($"소음기 장착 ");
+                }
+                Console.WriteLine("");
+            }
+
+
+        }
+        public class M24 : Gun
+        {
+            public M24(string name, string ammotype, int ammoMax, int damage, double firerate, bool attachScope, bool attachGrip, bool attachSuprresor)
+            {
+                Name = name;
+                Ammotype = ammotype;
+                AmmoMax = ammoMax;
+                Damage = damage;
+                Firerate = firerate;
+                AttachScope = attachScope;
+                AttachGrip = attachGrip;
+                AttachSuprresor = attachSuprresor;
+
+                Console.WriteLine($"{Ammotype}탄을 사용하는 {Name}입니다 ");
             }
 
 
 
+            public override void Attachment()
+            {
+                Console.Write($"{Name}에 ");
+
+                if (AttachScope == true)
+                {
+                    Console.Write($"8배율스코프 장착 ");
+                }
+
+                if (AttachGrip == true)
+                {
+                    Console.Write($"수직손잡이 장착 ");
+                }
+                if (AttachSuprresor == true)
+                {
+                    Console.Write($"소음기 장착 ");
+                }
+                Console.WriteLine("");
+            }
+
+        }
+        public class Beryl : Gun
+        {
+
+            public Beryl(string name, string ammotype, int ammoMax, int damage, double firerate, bool attachScope, bool attachGrip, bool attachSuprresor)
+            {
+                Name = name;
+                Ammotype = ammotype;
+                AmmoMax = ammoMax;
+                Damage = damage;
+                Firerate = firerate;
+                AttachScope = attachScope;
+                AttachGrip = attachGrip;
+                AttachSuprresor = attachSuprresor;
+
+                Console.WriteLine($"{Ammotype}탄을 사용하는 {Name}입니다");
+            }
+
+
+
+            public override void Attachment()
+            {
+                Console.Write($"{Name}에 ");
+
+                if (AttachScope == true)
+                {
+                    Console.Write($"8배율스코프 장착 ");
+                }
+
+                if (AttachGrip == true)
+                {
+                    Console.Write($"수직손잡이 장착 ");
+                }
+                if (AttachSuprresor == true)
+                {
+                    Console.Write($"소음기 장착 ");
+                }
+                Console.WriteLine("");
+            }
+           
         }
 
         // 오버로딩과 오버라이딩 
         static void Main(string[] args)
         {
+           Aug aug = new Aug("Aug","5.56mm",40,42,0.085d,false,true,true);
+           aug.Attachment();
+           aug.Reload();
+           aug.Shoot();
+           Console.WriteLine("");
+           M24 m24 = new M24("M24", "7.56mm", 7, 70, 1.4d, true, false, true);
+           m24.Attachment();
+           m24.Reload();  
+           m24.Shoot();
+           Console.WriteLine("");
+           Beryl beryl = new Beryl("Beryl", "7.56mm", 40, 45, 0.095d, true, false, true);
+           beryl.Reload();
+           beryl.Attachment();
+           beryl.Shoot();
+           
+             
+
+
+
+
         }
     }
 }
