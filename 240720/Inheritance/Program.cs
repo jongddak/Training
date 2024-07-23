9c3546c3e72acd78c14dba6a8984a11f7f337c97
jongddak <hfsmsj316@gmail.com>
2024-07-23 17:44:09 +0900

    feat gun class
    총기 기본 클래스 추가

--- 240720/Inheritance/Program.cs
+++ 240720/Inheritance/Program.cs
@@ -8,6 +8,33 @@ namespace Inheritance
 {
     internal class Program
     {
+
+        public class Gun {
+
+            public string Name;
+            public string Ammotype;
+            public int Damage;
+            public double Firerate;
+            bool AttachScope;
+
+            public virtual void shoot() 
+            {
+                Console.WriteLine("총알을 발사합니다");
+            }
+            public virtual void reload()
+            {
+                Console.WriteLine("총알을 재장전합니다");
+            }
+            public virtual void adjustment()
+            {
+                Console.WriteLine("조정간을 조정합니다");
+            }
+
+
+
+        }
+
+        // 오버로딩과 오버라이딩 
         static void Main(string[] args)
         {
         }
