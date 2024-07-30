5c6213fa5bd6d9f1e0a7cb2270c5683ce225a25c
jongddak <hfsmsj316@gmail.com>
2024-07-31 04:01:20 +0900

    요세푸스 순열
    요세푸스 순열

---
+++ 240731/Josephus/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Josephus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            LinkedList<int> list = new LinkedList<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.AddLast(i + 1);
+            }
+            int count = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                count++;
+                if (count == k)
+                {
+                    list
+                }
+            }
+        }
+    }
+}
