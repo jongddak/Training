779f6f79b1d1156532874e4f9645b30122b73af5
jongddak <hfsmsj316@gmail.com>
2024-07-31 22:28:37 +0900

    괄호체크 완성
    괄호체크완성

--- 240731/StackandQueue/Program.cs
+++ 240731/StackandQueue/Program.cs
@@ -2,17 +2,48 @@
 {
     internal class Program
     {
-        public static void GwalhoChecker(string str) {
-                
-           string[] splited = str.Split("");
-           
-           
+        public static void IsValid(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                if (ch == '{' || ch == '[' || ch == '(')
+                {
+                    stack.Push(ch);
+                }
+                else if (ch == '}' || ch == ']' || ch == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("미완");
+
+                    }
+
+                    char open = stack.Pop();
+
+                    if (!IsMatchingPair(open, ch))
+                    {
+                        Console.WriteLine("미완"); 
+                    }
+                }
+            }
+
+            Console.WriteLine("완"); 
         }
 
-        static void Main(string[] args)
+        static bool IsMatchingPair(char open, char close)
         {
-            string str = Console.ReadLine();
-            GwalhoChecker(str);
+            return (open == '{' && close == '}') ||
+                   (open == '[' && close == ']') ||
+                   (open == '(' && close == ')');
+        }
+    
+
+    static void Main(string[] args)
+        {
+           string str = Console.ReadLine();
+            IsValid(str);
 
 
 
